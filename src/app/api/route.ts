d7843c33e9e666b704d01c7371c69c0915779ac8
Pablo Marcano <devops@ia2030.com>
2024-05-22 15:25:45 -0400

    listos los cambios

--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server"
-
-export async function GET(request: NextRequest) {
-    const searchParams = new URLSearchParams(request.nextUrl.searchParams)
-    const code = searchParams.get('code')
-    const res = await fetch(`https://ep443.premierpluss.com/api-v1/tickets/${code}`)
-    return Response.json(await res.json())
-}
--- src/services/getTicketData.ts
+++ src/services/getTicketData.ts
@@ -1,6 +1,15 @@
-const handle = async (code: any) => {
-    const response = await fetch(`http://localhost:3000/api?code=${code}`, { cache: 'no-store' })
-    return await response.json()
+// const handle = async (code: any) => {
+//     const response = await fetch(`http://localhost:3000/api?code=${code}`, { cache: 'no-store' })
+//     return await response.json()
+// }
+
+// export default handle
+//import { NextRequest } from "next/server"
+
+export async function handle(code: any) {
+    const res = await fetch(`https://ep443.premierpluss.com/api-v1/tickets/${code}`)
+    //console.log(await res.json())
+    return await res.json()
 }
 
 export default handle
